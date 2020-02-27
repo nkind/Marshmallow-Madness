1a336174b64ca72e661d8b0964b17719e18cc329
Nick Kindrachuk <nkindrachuk@gmail.com>
2020-02-26 21:25:25 -0600

    Added sprites and basic player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+
+    [SerializeField]
+    private float yVal;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y != 4)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + yVal);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y != -4)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - yVal);
+            transform.position = targetPos;
+        }
+    }
+}
